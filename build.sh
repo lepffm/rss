a7b5cea42536724d5a51276a07f05f64680cce50
lbonanomi <5369016+lbonanomi@users.noreply.github.com>
2019-10-05 20:23:17 -0400

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,6 +1,11 @@
+# Get the last 100 updated repos under the defined organizations,
+# Get the last 100 issues labelled "Help Wanted" and format as
+# RSS articles
+
 ORGS="github grafana awslabs"
 
 get_issues() {
+    # Isn't this hideous?
     curl -s  -H "Authorization: bearer $TOKEN" -X POST -d '{ "query": "query { organization(login: \"'$ORG'\") { repositories(last: 100, orderBy: {field: UPDATED_AT, direction: ASC}) { edges { node { issues(labels: \"Help Wanted\", last: 100) { nodes { url } } } } } } }" }'  https://api.github.com/graphql | jq . | awk -F'"' '/url/ { print $4 }'  | while read ISSUE
     do
         printf "<item>\n<title>\n"
@@ -10,6 +15,9 @@ get_issues() {
         printf "</link>\n</item>\n"
     done
 }
+
+# Add some boilerplate to arranged "articles" into a coherent feed,
+# base64-encode it and smash it into a single line
 
 (
     echo '<?xml version="1.0" encoding="UTF-8" ?>'
@@ -23,8 +31,8 @@ get_issues() {
     printf "\n</channel>\n</rss>\n"
 ) | base64 | tr -d "\n" > feed.xml
 
-
+# Harvest current SHAof feed.xml
 CURRENT_SHA=$(curl -s -u :$TOKEN https://api.github.com/repos/lbonanomi/rss/contents/feed.xml | jq .sha | tr -d '"')
 
-curl -s -u :$TOKEN -X PUT -d '{ "message":"RSS Refresh Activity", "sha":"'$CURRENT_SHA'", "content":"'$(cat feed.xml)'" }' https://api.github.com/repos/lbonanomi/rss/contents/feed.xml #| jq .sha
-
+# Push feed.xml to Github
+curl -s -u :$TOKEN -X PUT -d '{ "message":"RSS Refresh Activity", "sha":"'$CURRENT_SHA'", "content":"'$(cat feed.xml)'" }' https://api.github.com/repos/lbonanomi/rss/contents/feed.xml
